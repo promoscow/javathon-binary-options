71c3621eb2c95ec4f4af6ffeb98c0c636f8dea32
SHARAFARCHI <stcon@mail.ru>
2019-01-27 01:22:59 +0300

    add CurrencyCode

---
+++ src/main/java/ru/xpendence/javathonbinaryoptions/service/CurrencyCode.java
@@ -0,0 +1,21 @@
+package ru.xpendence.javathonbinaryoptions.service;
+
+/**
+ * Author: Artur Sharafutdinov
+ * Date: 26.01.19
+ * Time: 18:44
+ * e-mail: sharhack@yahoo.com
+ */
+public enum CurrencyCode {
+    BTC,
+    ETH,
+    XRP,
+    LTC,
+    WAVES,
+    USDRUB,
+    EURRUB,
+    NZDUSD,
+    AUDUSD,
+    USDJPY
+
+}
